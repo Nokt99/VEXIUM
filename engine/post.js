731a4560d3c672e18f147ac0bf640ab27ecfa94a
GibberFish <zakariyah6204@mytusd.org>
2026-02-11 15:35:10 -0800

    Update post.js

--- engine/post.js
+++ engine/post.js
@@ -1,3 +1,7 @@
+import {PixelNoiseShader} from './shaders/pixelNoise.js'
+import {BlueStainShader} from './shaders/blueStain.js'
+import {CorruptionShader} from './shaders/corruption.js'
+
 export function addPost(renderer,scene,camera){
 
 const composer=new THREE.EffectComposer(renderer)
@@ -7,11 +11,25 @@ composer.addPass(renderPass)
 
 const bloom=new THREE.UnrealBloomPass(
 new THREE.Vector2(window.innerWidth,window.innerHeight),
-.8,
-.4,
+.9,
+.5,
 .85
 )
 composer.addPass(bloom)
+
+const bluePass=new THREE.ShaderPass(BlueStainShader)
+composer.addPass(bluePass)
+
+const noisePass=new THREE.ShaderPass(PixelNoiseShader)
+composer.addPass(noisePass)
+
+const corruptPass=new THREE.ShaderPass(CorruptionShader)
+composer.addPass(corruptPass)
+
+composer.userData={
+noisePass,
+corruptPass
+}
 
 return composer
 }
