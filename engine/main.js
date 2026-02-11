824de47efb6c7b06b36c288c641925eddff2cae1
GibberFish <zakariyah6204@mytusd.org>
2026-02-11 15:35:40 -0800

    Update main.js

--- engine/main.js
+++ engine/main.js
@@ -1,22 +1,41 @@
 import {createScene} from './scene.js'
 import {createKnight} from '../knight/knight.js'
 import {addPost} from './post.js'
-import {addGlitchSystem} from './glitch.js'
 
 const {scene,camera,renderer}=createScene()
 const knight=createKnight()
 scene.add(knight)
 
 const composer=addPost(renderer,scene,camera)
-addGlitchSystem(composer,knight)
 
 let clock=new THREE.Clock()
 
 function animate(){
 requestAnimationFrame(animate)
+
 let t=clock.getElapsedTime()
+
 knight.userData.update(t)
+
+composer.userData.noisePass.uniforms.time.value=t
+composer.userData.corruptPass.uniforms.time.value=t
+
 composer.render()
 }
 
 animate()
+
+setInterval(()=>{
+let level=Math.random()
+if(level>.75){
+composer.userData.corruptPass.uniforms.intensity.value=1.0
+setTimeout(()=>{
+composer.userData.corruptPass.uniforms.intensity.value=.4
+},200)
+}else{
+composer.userData.corruptPass.uniforms.intensity.value=.5
+setTimeout(()=>{
+composer.userData.corruptPass.uniforms.intensity.value=.2
+},120)
+}
+},6000+Math.random()*5000)
